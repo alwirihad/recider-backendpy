bd16a8ad430407eddaba3249e77f4a4a2a843cdc
Alwi Rihad <alwirihad8@gmail.com>
2023-12-16 22:32:33 +0700

    modified:   app.py 	new file:   recipe_knn_model.joblib

--- app.py
+++ app.py
@@ -1,57 +1,65 @@
 import os
 from flask import Flask, request, jsonify
-import joblib
 import pandas as pd
-from flask_cors import CORS
+import joblib
+import numpy as np
+
 
 app = Flask(__name__)
-CORS(app)
 
-# Load the pre-trained model and vectorizer
-knn_model = joblib.load("./models/knn_model.joblib")
-vectorizer = joblib.load('./models/tfidf_vectorizer.joblib')
+# Load the model and feature names
+model_data = joblib.load('recipe_knn_model.joblib')
+knn_model = model_data['model']
+feature_names = model_data['feature_names']
 
-csv_path = os.environ.get('CSV_PATH', './data/recido.csv')
-df = pd.read_csv(csv_path)
-
-
-# Endpoint for getting recipe recommendations
-@app.route('/')
-def index():
-    return 'Hello World!'
+folder_path = 'data'
+recipes_df = pd.read_csv(os.path.join(folder_path, 'recido.csv'))
 
 @app.route('/recommend', methods=['POST'])
-def recommend():
+def recommend_recipe():
     try:
-        data = request.get_json(force=True)
-        user_ingredients = data.get('ingredients', '').split(', ')
+        if not request.json or 'ingredients' not in request.json:
+            return jsonify({"error": "Invalid input. 'ingredients' key is required in the JSON request."}), 400
+        user_input_ingredients = [ingredient.lower() for ingredient in request.json.get('ingredients', [])]
 
-        if not user_ingredients:
-            raise ValueError("Input ingredients tidak boleh kosong.")
+        filtered_df = recipes_df
 
-        user_input = vectorizer.transform([" ".join(user_ingredients)])
+        for ingredient in user_input_ingredients:
+            lower_ingredient = ingredient.lower()
+            filtered_df = filtered_df[filtered_df['Ingredients'].str.lower().str.contains(lower_ingredient)]
 
-        distances, indices = knn_model.kneighbors(user_input)
+        if filtered_df.empty:
+            return jsonify({"message": "Resep makanan tidak ditemukan."}), 404
 
-        recommendations = get_recommendations(indices)
+        X_features = pd.get_dummies(filtered_df[['Title', 'Ingredients', 'Steps']], drop_first=True)
 
-        output = {'recommendations': recommendations}
-        status_code = 200
+        user_input_df = pd.DataFrame(0, columns=feature_names, index=[0])
+
+        for ingredient in user_input_ingredients:
+            if ingredient.lower() in feature_names:
+               user_input_df[ingredient.lower()] = 1
+
+        # Make predictions
+        distances, indices = knn_model.kneighbors(user_input_df)
+        filtered_indices = filtered_df.index.to_numpy()
+
+        indices = indices.flatten()
+        indices = indices[indices < len(filtered_df)]
+
+        if len(indices) == 0:
+            return jsonify({"message": "Resep makanan tidak ditemukan."}), 404
+
+        recommended_recipes = filtered_df.iloc[indices]
+
+        response_data = {
+            "message": "Resep Direkomendasikan",
+            "recipes": recommended_recipes.to_dict(orient='records')
+        }
+
+        return jsonify(response_data)
 
     except Exception as e:
-        output = {'error': str(e)}
-        status_code = 400
-
-    return jsonify(output), status_code
-
-def get_recommendations(indices):
-    recommended_indices = indices[0]
-
-    recommended_data = df.iloc[recommended_indices]
-
-    recommendations = [{'Title': row['Title'], 'Ingredients': row['Ingredients'], 'Steps': row['Steps']} for _, row in recommended_data.iterrows()]
-
-    return recommendations
+        return jsonify({"error": str(e)}), 400
 
 if __name__ == '__main__':
     app.run(debug=True)
